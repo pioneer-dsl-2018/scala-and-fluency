266f5cee722c93b961b9278650be6e7acc762d0b
Ben Wiedermann <benw@cs.hmc.edu>
2018-03-12 11:25:58 -0700

    More operations for the example program.

--- src/main/scala/pioneer/Program.scala
+++ src/main/scala/pioneer/Program.scala
@@ -3,6 +3,11 @@ package pioneer
 import pioneer.pictures.Pictures
 
 object Program extends App {
-  // Flip a picture horizontally and save the result to disk
-  Pictures.flipHorizontal(resource("/image.png"), "output.png")
+  // flip a picture horizontally, grayscale it, and rotate it left
+  Pictures.flipHorizontal(resource("/image.png"), "output0.png")
+  Pictures.grayScale("output0.png", "output1.png")
+  Pictures.rotateLeft("output1.png", "output2.png")
+
+  // The final picture is now in the top folder of this project's directory,
+  // in a file called "output2.png".
 }
